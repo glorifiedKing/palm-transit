bfe2be3fe8c20c691acb7f131999622dbd026e8b
Raymond Byaruhanga <11267178+glorifiedKing@users.noreply.github.com>
2020-03-06 16:10:48 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,5 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/mobilemoney/result', '\glorifiedking\BusTravel\Http\Controllers\ApiController@index')->name('bustravel.mmresult');
