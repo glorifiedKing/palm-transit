07616c9a588ef4a8a071258c92f316538e17a960
Raymond Byaruhanga <11267178+glorifiedKing@users.noreply.github.com>
2020-04-08 18:19:38 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ Route::get('/bt-add-cart/{id}/{date}/{type?}', '\glorifiedking\BusTravel\Http\Co
 Route::get('/bt-cart', '\glorifiedking\BusTravel\Http\Controllers\FrontendController@cart')->name('bustravel.cart');
 Route::get('/bt-clear-cart','\glorifiedking\BusTravel\Http\Controllers\FrontendController@clear_cart')->name('bustravel.cart.clear');
 Route::any('/payment/callback','\glorifiedking\BusTravel\Http\Controllers\FrontendController@process_payment_callback')->name('bustravel.payment.callback');
+Route::any('/payment/credit_callback','\glorifiedking\BusTravel\Http\Controllers\FrontendController@credit_request_callback')->name('bustravel.credit.callback');
 Route::get('/bt-remove-cart-item/{id}','\glorifiedking\BusTravel\Http\Controllers\FrontendController@remove_cart_item')->name('bustravel.cart.remove.item');
 Route::get('/bt-checkout','\glorifiedking\BusTravel\Http\Controllers\FrontendController@checkout')->name('bustravel.cart.checkout');
 Route::post('/bt-checkout','\glorifiedking\BusTravel\Http\Controllers\FrontendController@process_payment')->name('bustravel.cart.pay');
