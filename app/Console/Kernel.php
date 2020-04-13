b9ac5a27712c520d50eb77ca14b1d42dcf3cda37
Raymond Byaruhanga <11267178+glorifiedKing@users.noreply.github.com>
2020-04-13 16:24:41 +0300

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Console;
 
+use glorifiedking\BusTravel\Commands\ClearDebitTransactions;
+use glorifiedking\BusTravel\Commands\ClearCreditTransactions;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -13,7 +15,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        ClearDebitTransactions::class,
+	    ClearCreditTransactions::class,
     ];
 
     /**
