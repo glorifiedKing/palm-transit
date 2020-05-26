dadb278be97409ca2f93d6143a42d2f86c92b2b6
Raymond Byaruhanga <11267178+glorifiedKing@users.noreply.github.com>
2020-05-26 23:45:40 +0300

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/transit/routes';
+    protected $redirectTo = '/transit/dashboard';
 
     /**
      * Create a new controller instance.
