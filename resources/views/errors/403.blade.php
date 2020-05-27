6891757686fb2b53e2ccc60d9b3736674fe3b02e
Raymond Byaruhanga <11267178+glorifiedKing@users.noreply.github.com>
2020-05-27 13:51:29 +0300

    Create 403.blade.php

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,16 @@
+@extends('bustravel::frontend.layouts.app')
+@section('title', 'Unauthorized access')
+@section('page-heading','Unauthorized Access')    
+
+
+            @section('content')
+                <div class="row">
+                    <div class="col-md-8">
+                        <h1 class="h3 mb-3 font-weight-normal">Un Authorized access!</h1>
+                        <div class="alert alert-danger" role="alert">
+			  <strong>Error!</strong> Access to this resource is not Authorized. Contact Administrator
+			</div>
+                       
+                    </div>
+                </div>
+                @endsection
