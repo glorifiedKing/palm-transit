7be1a4ce10abc5dbafa6d618e11ae38247048dee
Raymond <raymond.byaruhanga@ingenious.build>
2025-04-28 11:30:58 +0300

    add read and write

--- config/database.php
+++ config/database.php
@@ -61,6 +61,17 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
+            'read' => [
+                'host' => [
+                    env('DB_READ_HOST', 'read-replica-1.endpoint.rds.amazonaws.com'),
+                    env('DB_READ_HOST_2', 'read-replica-2.endpoint.rds.amazonaws.com'),
+                ],
+            ],
+            'write' => [
+                'host' => [
+                    env('DB_WRITE_HOST', 'master.endpoint.rds.amazonaws.com'),
+                ],
+            ],
         ],
 
         'pgsql' => [
@@ -123,7 +134,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
